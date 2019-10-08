133ed56ec2b4cfee8831333d6759988632a326c5
Matta-Aaron <37586057+Matta-Aaron@users.noreply.github.com>
2019-10-07 23:14:28 -0600

    Menusagregados
    Cambios por Aarón y Jesús
    
    Co-Authored-By: jesustorresrodriguez <jesustorresrodriguez@users.noreply.github.com>

---
+++ GestionPersonalOIJ/Controllers/ProcesoSeleccionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GestionPersonalOIJ.Models;
+
+namespace GestionPersonalOIJ.Controllers
+{
+    public class ProcesoSeleccionController : Controller
+    {
+        // GET: /<controller>/
+
+        public IActionResult PrimerosIngresos()
+        {
+            return View();
+        }
+
+    }
+}
--- GestionPersonalOIJ/Startup.cs
+++ GestionPersonalOIJ/Startup.cs
@@ -68,6 +68,10 @@ namespace GestionPersonalOIJ
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "second",
+                    template: "{controller=ProcesoSeleccion}/{action=PrimerosIngresos}/{id?}");
             });
         }
     }
