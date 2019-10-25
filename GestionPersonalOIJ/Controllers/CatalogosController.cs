b5e066cb6a5a2f85f1c64dea254d31c1ca01d78d
Manfred99 <manfredgh@hotmail.com>
2019-10-24 23:49:38 -0600

    CarteleraCommit1

---
+++ GestionPersonalOIJ/Controllers/CatalogosController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GestionPersonalOIJ.Models;
+using MOGESP.ServiceLayer.Servicio;
+using MOGESP.Entities.Dominio;
+using Microsoft.AspNetCore.Http;
+using MOGESP.Entities.Utilidades;
+
+namespace GestionPersonalOIJ.Controllers
+{
+    public class CatalogosController : Controller
+    {
+
+     
+
+		public IActionResult CarteleraCatalogos()
+		{
+			return View();
+		}
+
+		//public IActionResult VerPrimerosIngresos()
+		//{
+		//    IEnumerable<PrimerIngreso> primerosIngresos;
+		//    primerosIngresos = primerIngresoServicio.getAllPrimerosIngresos();
+		//    return View(primerosIngresos);
+		//}
+
+    }
+}
