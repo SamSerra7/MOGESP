9222a027efa7cfb7e99f81d4ab9f44834500c2a3
Samuel <samsserranoguerra7@gmail.com>
2019-11-07 00:45:30 -0600

    Ascensos
    Entidad,Data,Servicio,UI

--- GestionPersonalOIJ/Controllers/AscensosController.cs
+++ GestionPersonalOIJ/Controllers/AscensosController.cs
@@ -2,15 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MOGESP.ServiceLayer.Servicio;
+using MOGESP.Entities.Dominio;
+using MOGESP.Entities.Utilidades;
 
 namespace MOGESP.UserInterface.Controllers
 {
     public class AscensosController : Controller
     {
-        public IActionResult AgregarConcurso()
+
+        readonly FuncionarioServicio funcioniarioServicio = new FuncionarioServicio();
+        private readonly int registrosPorPagina = 10;
+        private List<Funcionario> funcionarios = new List<Funcionario>();
+        private PaginadorGenerico<Funcionario> paginador;
+
+
+
+
+        public ActionResult VerFuncionarios(int pagina = 1, String buscar = "")
         {
-            return View();
+
+            IEnumerable<Funcionario> funcionarios = primerIngresoServicio.ListarFuncionarios();
+            int totalRegistros = 0;
+
+            // FILTRO DE BÚSQUEDA
+            // Filtramos el resultado por el 'texto de búqueda'
+            if (!string.IsNullOrEmpty(buscar))
+            {
+                foreach (var item in buscar.Split(new char[] { ' ' },
+                         StringSplitOptions.RemoveEmptyEntries))
+                {
+                    funcionarios = funcionarios.Where(x => x.Cedula.Contains(item) ||
+                                                      x.Nombre.Contains(item) ||
+                                                      x.PrimerApellido.Contains(item) ||
+                                                      x.SegundoApellido.Contains(item))
+                                                      .ToList();
+                }
+            }
+
+            // Obtenemos la 'página de registros' de la tabla 
+            funcionarios = funcionarios.OrderBy(x => x.Cedula)
+                                                 .Skip((pagina - 1) * registrosPorPagina)
+                                                 .Take(registrosPorPagina)
+                                                 .ToList();
+            // Número total de registros de la tabla		
+            totalRegistros = funcionarios.Count();
+
+
+            // Número total de páginas de la tabla 
+            var _TotalPaginas = (int)Math.Ceiling((double)totalRegistros / registrosPorPagina);
+            // Instanciamos la 'Clase de paginación' y asignamos los nuevos valores
+            paginador = new PaginadorGenerico<Funcionario>()
+            {
+                RegistrosPorPagina = registrosPorPagina,
+                TotalRegistros = totalRegistros,
+                TotalPaginas = _TotalPaginas,
+                PaginaActual = pagina,
+                Resultado = funcionarios
+            };
+            // Enviamos a la Vista la 'Clase de paginación'
+            return View(paginador);
         }
+
+
     }
 }
---
+++ MOGESP.DataAccess.TRAN/Datos/FuncionariosDatos.cs
@@ -0,0 +1,117 @@
+﻿using MOGESP.Entities.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace MOGESP.DataAccess.TRAN.Datos
+{
+    /// <summary>
+    /// Clase que administra los funcionarios
+    /// </summary>
+    public class FuncionariosDatos
+    {
+        //variable conexion
+        private ConexionDatos conexion = new ConexionDatos();
+
+        //variables
+        private CorreoDatos correoDatos = new CorreoDatos();
+        private TelefonoDatos telefonoDatos = new TelefonoDatos();
+
+        /// <summary>
+        /// Autores: Samuel 
+        /// 7/11/19
+        /// Este método retorna una lista con todos los funcionarios.
+        /// </summary>
+        /// <returns>List<Funcionario></returns>
+        public List<Funcionario> ListarFuncionarios()
+        {
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            SqlConnection sqlConnection = conexion.conexion();
+
+            SqlCommand sqlCommand = new SqlCommand(@"EXEC PA_ConsultarFuncionarios", sqlConnection);
+
+            SqlDataReader reader;
+            sqlConnection.Open();
+            reader = sqlCommand.ExecuteReader();
+
+            PrimerIngreso primerIngreso;
+
+            while (reader.Read())
+            {
+                primerIngreso = new PrimerIngreso();
+			
+                primerIngreso.Cedula = reader["TC_NumeroCedula"].ToString();
+                primerIngreso.Nombre = reader["TC_Nombre"].ToString();
+                primerIngreso.PrimerApellido = reader["TC_PrimerApellido"].ToString();
+                primerIngreso.SegundoApellido = reader["TC_SegundoApellido"].ToString();
+                primerIngreso.Sexo = Convert.ToChar(reader["TC_Sexo"].ToString());
+                primerIngreso.Direccion = reader["TC_Direccion"].ToString();
+                primerIngreso.NumeroConvocatoria = reader["TC_NumeroConvocatoria"].ToString();
+                primerIngreso.NumeroFlujo = Convert.ToInt32(reader["TN_NumeroFlujo"].ToString());
+
+                primerIngreso.Correos = correoDatos.CosultarCorreosPorPrimerIngreso(primerIngreso.Cedula);
+                primerIngreso.Telefonos = telefonoDatos.CosultarTelefonosPorPrimerIngreso(primerIngreso.Cedula);
+
+
+                primerosIngresos.Add(primerIngreso);
+            }
+
+            sqlConnection.Close();
+
+            return primerosIngresos;
+        }
+
+
+
+        /// <summary>
+        /// Autores: Samuel 
+        /// 10/10/19
+        /// Este método ingresa un primer ingreso.
+        /// </summary>
+        public void InsertarFuncionario(PrimerIngreso primerIngreso)
+        {
+
+            SqlConnection sqlConnection = conexion.conexion();
+
+            SqlCommand insertarPI = new SqlCommand(@"EXEC PA_InsertarPrimerosIngresos @NumeroConvocatoria, @NumeroFlujo, @Cedula, @Nombre, 
+                                                        @PrimerApellido, @SegundoApellido, @Direccion, @Sexo", sqlConnection);
+
+
+            insertarPI.Parameters.AddWithValue("@Cedula", primerIngreso.Cedula);
+            insertarPI.Parameters.AddWithValue("@Nombre", primerIngreso.Nombre);
+            insertarPI.Parameters.AddWithValue("@PrimerApellido", primerIngreso.PrimerApellido);
+            insertarPI.Parameters.AddWithValue("@SegundoApellido", primerIngreso.SegundoApellido);
+            insertarPI.Parameters.AddWithValue("@Sexo", primerIngreso.Sexo);
+            insertarPI.Parameters.AddWithValue("@Direccion", primerIngreso.Direccion);
+            insertarPI.Parameters.AddWithValue("@NumeroConvocatoria", primerIngreso.NumeroConvocatoria);
+            insertarPI.Parameters.AddWithValue("@NumeroFlujo", primerIngreso.NumeroFlujo);
+
+            sqlConnection.Open();
+            insertarPI.ExecuteReader();
+
+            if (primerIngreso.Correos.Count != 0) { 
+                foreach (var correo in primerIngreso.Correos)
+                {
+                    correoDatos.insertarCorreoPorPersona(primerIngreso.Cedula,correo);
+                }
+            }
+
+            if (primerIngreso.Telefonos.Count != 0)
+            {
+                foreach (var telefono in primerIngreso.Telefonos)
+                {
+                    telefonoDatos.insertarTelefonoPorPersona(primerIngreso.Cedula, telefono);
+                }
+            }
+
+            sqlConnection.Close();
+            insertarPI.Dispose();
+        }
+
+
+
+
+    }
+}
