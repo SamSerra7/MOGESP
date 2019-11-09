6cef88ca588e8a39a76578e28898f7d2e5d8940b
Matta-Aaron <37586057+Matta-Aaron@users.noreply.github.com>
2019-11-09 01:19:24 -0600

    PI_Departamentos_Create

---
+++ Model/Dominio/DepartamentoPruebasGH.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOGESP.Entities.Dominio
+{
+    public class DepartamentoPruebasGH
+    {
+
+        public DepartamentoPruebasGH()
+        {
+        }
+
+        public DepartamentoPruebasGH(string numeroCedula, DateTime fechaIngresoAdministracion, int cantidadDiasAdministracion, DateTime fechaIngreso, string oficioIngreso, int diasALaFecha, DateTime fechaTrasladoPsicologosAdmin, string numOficio, DateTime fechaDevolucionGHDeAdmin, int cantidadDiasPsicologiaAdmin, DateTime fechaLimiteSegunPlazo, int diasALaFechaDeFechaLimiteSegunPlazo, int diasTramiteGHDespuesDevuelto, DateTime fechaSalida, int cantidadDiasTotalesTramite, string oficioRespuesta, int estadoResultHojaEnvioGH)
+        {
+            NumeroCedula = numeroCedula ?? throw new ArgumentNullException(nameof(numeroCedula));
+            FechaIngresoAdministracion = fechaIngresoAdministracion;
+            CantidadDiasAdministracion = cantidadDiasAdministracion;
+            FechaIngreso = fechaIngreso;
+            OficioIngreso = oficioIngreso;
+            DiasALaFecha = diasALaFecha;
+            FechaTrasladoPsicologosAdmin = fechaTrasladoPsicologosAdmin;
+            NumOficio = numOficio;
+            FechaDevolucionGHDeAdmin = fechaDevolucionGHDeAdmin;
+            CantidadDiasPsicologiaAdmin = cantidadDiasPsicologiaAdmin;
+            FechaLimiteSegunPlazo = fechaLimiteSegunPlazo;
+            DiasALaFechaDeFechaLimiteSegunPlazo = diasALaFechaDeFechaLimiteSegunPlazo;
+            DiasTramiteGHDespuesDevuelto = diasTramiteGHDespuesDevuelto;
+            FechaSalida = fechaSalida;
+            CantidadDiasTotalesTramite = cantidadDiasTotalesTramite;
+            OficioRespuesta = oficioRespuesta;
+            EstadoResultHojaEnvioGH = estadoResultHojaEnvioGH;
+        }
+
+        private string numeroCedula;
+        public string NumeroCedula
+        {
+            get { return numeroCedula; }
+            set
+            {
+                if (value == null || value.Trim().Equals("")) throw new Exception("El campo de numeroCedula es requerido");
+                numeroCedula = value;
+            }
+        }
+
+        private DateTime fechaIngresoAdministracion;
+        public DateTime FechaIngresoAdministracion
+        {
+            get { return fechaIngresoAdministracion; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de fechaIngresoAdministracion es requerido");
+                fechaIngresoAdministracion = value;
+            }
+        }
+
+        private int cantidadDiasAdministracion;
+        public int CantidadDiasAdministracion
+        {
+            get { return cantidadDiasAdministracion; }
+            set
+            {
+                if (value < 0) throw new Exception("El campo de cantidadDiasAdministracion es requerido");
+                cantidadDiasAdministracion = value;
+            }
+        }
+
+        private DateTime fechaIngreso;
+        public DateTime FechaIngreso
+        {
+            get { return fechaIngreso; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de fechaIngreso es requerido");
+                fechaIngreso = value;
+            }
+        }
+
+        private string oficioIngreso;
+        public string OficioIngreso
+        {
+            get { return oficioIngreso; }
+            set
+            {
+                oficioIngreso = value ?? throw new Exception("El campo de oficioIngreso es requerido");
+            }
+        }
+
+        private int diasALaFecha;
+        public int DiasALaFecha
+        {
+            get { return diasALaFecha; }
+            set
+            {
+                if (value < 0) throw new Exception("El campo de diasALaFecha es requerido");
+                diasALaFecha = value;
+            }
+        }
+
+        private DateTime fechaTrasladoPsicologosAdmin;
+        public DateTime FechaTrasladoPsicologosAdmin
+        {
+            get { return fechaTrasladoPsicologosAdmin; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de fechaTrasladoPsicologosAdmin es requerido");
+                fechaTrasladoPsicologosAdmin = value;
+            }
+        }
+
+        private string numOficio;
+        public string NumOficio
+        {
+            get { return numOficio; }
+            set
+            {
+                numOficio = value ?? throw new Exception("El campo de numOficio es requerido");
+            }
+        }
+
+        private DateTime fechaDevolucionGHDeAdmin;
+        public DateTime FechaDevolucionGHDeAdmin
+        {
+            get { return fechaDevolucionGHDeAdmin; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de fechaDevolucionGHDeAdmin es requerido");
+                fechaDevolucionGHDeAdmin = value;
+            }
+        }
+
+        private int cantidadDiasPsicologiaAdmin;
+        public int CantidadDiasPsicologiaAdmin
+        {
+            get { return cantidadDiasPsicologiaAdmin; }
+            set
+            {
+                if (value < 0) throw new Exception("El campo de cantidadDiasPsicologiaAdmin es requerido");
+                cantidadDiasPsicologiaAdmin = value;
+            }
+        }
+
+        private DateTime fechaLimiteSegunPlazo;
+        public DateTime FechaLimiteSegunPlazo
+        {
+            get { return fechaLimiteSegunPlazo; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de fechaLimiteSegunPlazo es requerido");
+                fechaLimiteSegunPlazo = value;
+            }
+        }
+
+        private int diasALaFechaDeFechaLimiteSegunPlazo;
+        public int DiasALaFechaDeFechaLimiteSegunPlazo
+        {
+            get { return diasALaFechaDeFechaLimiteSegunPlazo; }
+            set
+            {
+                if (value < 0) throw new Exception("El campo de diasALaFechaDeFechaLimiteSegunPlazo es requerido");
+                diasALaFechaDeFechaLimiteSegunPlazo = value;
+            }
+        }
+
+        private int diasTramiteGHDespuesDevuelto;
+        public int DiasTramiteGHDespuesDevuelto
+        {
+            get { return diasTramiteGHDespuesDevuelto; }
+            set
+            {
+                if (value < 0) throw new Exception("El campo de diasTramiteGHDespuesDevuelto es requerido");
+                diasTramiteGHDespuesDevuelto = value;
+            }
+        }
+
+        private DateTime fechaSalida;
+        public DateTime FechaSalida
+        {
+            get { return fechaSalida; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de fechaSalida es requerido");
+                fechaSalida = value;
+            }
+        }
+
+        private int cantidadDiasTotalesTramite;
+        public int CantidadDiasTotalesTramite
+        {
+            get { return cantidadDiasTotalesTramite; }
+            set
+            {
+                if (value < 0) throw new Exception("El campo de cantidadDiasTotalesTramite es requerido");
+                cantidadDiasTotalesTramite = value;
+            }
+        }
+
+        private string oficioRespuesta;
+        public string OficioRespuesta
+        {
+            get { return oficioRespuesta; }
+            set
+            {
+                oficioRespuesta = value ?? throw new Exception("El campo de oficioRespuesta es requerido");
+            }
+        }
+
+        private int estadoResultHojaEnvioGH;
+        public int EstadoResultHojaEnvioGH
+        {
+            get { return estadoResultHojaEnvioGH; }
+            set
+            {
+                if (value < 0) throw new Exception("El campo de estadoResultHojaEnvioGH es requerido");
+                estadoResultHojaEnvioGH = value;
+            }
+        }
+
+    }
+
+
+
+}
---
+++ Model/Dominio/PrimerIngresoDepartamentos.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOGESP.Entities.Dominio
+{
+    public class PrimerIngresoDepartamentos
+    {
+
+        public PrimerIngresoDepartamentos()
+        {
+            
+        }
+
+        public PrimerIngresoDepartamentos(string numeroCedula, string nombrePI, string primerApellido, string segundoApellido, DepartamentoPruebasGH departamentoPruebas, DepartamentoAntecedentes departamentoAntecedentes, DepartamentoPruebasMedicas departamentoPruebasMedicas, DepartamentoVialidad departamentoVialidad, DepartamentoToxicologia departamentoToxicologia)
+        {
+            NumeroCedula = numeroCedula ?? throw new ArgumentNullException(nameof(numeroCedula));
+            NombrePI = nombrePI ?? throw new ArgumentNullException(nameof(nombrePI));
+            PrimerApellido = primerApellido ?? throw new ArgumentNullException(nameof(primerApellido));
+            SegundoApellido = segundoApellido ?? throw new ArgumentNullException(nameof(segundoApellido));
+            DepartamentoPruebas = departamentoPruebas ?? throw new ArgumentNullException(nameof(departamentoPruebas));
+            DepartamentoAntecedentes = departamentoAntecedentes ?? throw new ArgumentNullException(nameof(departamentoAntecedentes));
+            DepartamentoPruebasMedicas = departamentoPruebasMedicas ?? throw new ArgumentNullException(nameof(departamentoPruebasMedicas));
+            DepartamentoVialidad = departamentoVialidad ?? throw new ArgumentNullException(nameof(departamentoVialidad));
+            DepartamentoToxicologia = departamentoToxicologia ?? throw new ArgumentNullException(nameof(departamentoToxicologia));
+        }
+
+
+
+        /*Informacion General del Primer Ingreso*/
+        private string numeroCedula;
+        public string NumeroCedula
+        {
+            get { return numeroCedula; }
+            set
+            {
+                if (value == null || value.Trim().Equals("")) throw new Exception("El campo de numeroCedula es requerido");
+                numeroCedula = value;
+            }
+        }
+        private string nombrePI;
+        public string NombrePI
+        {
+            get { return nombrePI; }
+            set
+            {
+                if (value == null || value.Trim().Equals("")) throw new Exception("El campo de nombrePI es requerido");
+                nombrePI = value;
+            }
+        }
+        private string primerApellido;
+        public string PrimerApellido
+        {
+            get { return primerApellido; }
+            set
+            {
+                if (value == null || value.Trim().Equals("")) throw new Exception("El campo de primerApellido es requerido");
+                primerApellido = value;
+            }
+        }
+        private string segundoApellido;
+        public string SegundoApellido
+        {
+            get { return segundoApellido; }
+            set
+            {
+                if (value == null || value.Trim().Equals("")) throw new Exception("El campo de segundoApellido es requerido");
+                segundoApellido = value;
+            }
+        }
+
+        /*Informacion Tabla PruebasGH*/
+        private DepartamentoPruebasGH departamentoPruebas;
+        public DepartamentoPruebasGH DepartamentoPruebas
+        {
+            get { return departamentoPruebas; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de departamentoPruebas es requerido");
+                departamentoPruebas = value;
+            }
+        }
+
+        /*Informacion Tabla Antecedentes*/
+        private DepartamentoAntecedentes departamentoAntecedentes;
+        public DepartamentoAntecedentes DepartamentoAntecedentes
+        {
+            get { return departamentoAntecedentes; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de departamentoPruebas es requerido");
+                departamentoAntecedentes = value;
+            }
+        }
+
+        /*Informacion Tabla PruebasMedicas*/
+        private DepartamentoPruebasMedicas departamentoPruebasMedicas;
+        public DepartamentoPruebasMedicas DepartamentoPruebasMedicas
+        {
+            get { return departamentoPruebasMedicas; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de departamentoPruebas es requerido");
+                departamentoPruebasMedicas = value;
+            }
+        }
+
+        /*Informacion Tabla Vialidad*/
+        private DepartamentoVialidad departamentoVialidad;
+        public DepartamentoVialidad DepartamentoVialidad
+        {
+            get { return departamentoVialidad; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de departamentoPruebas es requerido");
+                departamentoVialidad = value;
+            }
+        }
+
+        /*Informacion Tabla Toxicologia*/
+        private DepartamentoToxicologia departamentoToxicologia;
+        public DepartamentoToxicologia DepartamentoToxicologia
+        {
+            get { return departamentoToxicologia; }
+            set
+            {
+                if (value == null) throw new Exception("El campo de departamentoPruebas es requerido");
+                departamentoToxicologia = value;
+            }
+        }
+
+    }
+}
