340933157836ae963faf8547f2c4053a2d0eb217
Manfred99 <manfredgh@hotmail.com>
2019-11-10 17:09:12 -0600

    CommitConexion

--- MOGESP.DataAccess.TRAN/Datos/ConexionDatos.cs
+++ MOGESP.DataAccess.TRAN/Datos/ConexionDatos.cs
@@ -10,8 +10,9 @@ namespace MOGESP.DataAccess.TRAN.Datos
 
         public SqlConnection conexion()
         {
-            String conexion = "Server=163.178.173.148;Database=MOGESPProd;User id=estudiantesrp;Password=estudiantesrp;";
-            conexion = "Server=DESKTOP-QI4QP2A\\SQLEXPRESS;Database=MOGESP;User id=usrMOGESP;Password=usrMOGESP;"; 
+            //String conexion = "Server=163.178.173.148;Database=MOGESPProd;User id=estudiantesrp;Password=estudiantesrp;";//paraiso
+            //String conexion = "Server=DESKTOP-QI4QP2A\\SQLEXPRESS;Database=MOGESP;User id=usrMOGESP;Password=usrMOGESP;"; //Manfred local
+            String conexion = "Server=163.178.107.130;Database=MOGESP;User id=laboratorios;Password=Saucr.118P;";//Turrialba
             return new SqlConnection(conexion);
         }
 
