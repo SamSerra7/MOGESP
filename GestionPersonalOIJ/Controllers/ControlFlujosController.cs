fb20ab5bd27b17d0c454e6cba4aa3a2bf1389870
Samuel <samsserranoguerra7@gmail.com>
2019-11-12 01:21:12 -0600

    FlujoSeleccion
    falta el data

---
+++ GestionPersonalOIJ/Controllers/ControlFlujosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MOGESP.UserInterface.Controllers
+{
+    public class ControlFlujosController : Controller
+    {
+
+
+        public ActionResult VerFlujoSeleccion()
+        {
+            
+
+
+
+
+            return View();
+        }
+        
+    }
+}
