0116c170488e642f53e9a3ce0fbe56222b3e6f03
Samuel <samsserranoguerra7@gmail.com>
2019-11-12 17:34:44 -0600

    Psicologos
    ControlFlujos

--- GestionPersonalOIJ/Controllers/ControlFlujosController.cs
+++ GestionPersonalOIJ/Controllers/ControlFlujosController.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MOGESP.ServiceLayer.Servicio;
+using MOGESP.Entities.Dominio;
+using MOGESP.Entities.Utilidades;
 
 namespace MOGESP.UserInterface.Controllers
 {
     public class ControlFlujosController : Controller
     {
+
+        //variables
+        private PrimerIngresoServicio primerIngresoServicio = new PrimerIngresoServicio();
+
+
+
+        public ActionResult ModificarResultadosPsicologos()
+        {
+            
+
+            return View(primerIngresoServicio.getAllPrimerosIngresos());
+        }
+
 
 
         public ActionResult VerFlujoSeleccion()
