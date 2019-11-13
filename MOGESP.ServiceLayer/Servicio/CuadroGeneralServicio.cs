51f80050c748f7650271d9fdbc0c41799fcedcd7
Manfred99 <manfredgh@hotmail.com>
2019-11-13 00:58:59 -0600

    ManfredCommit

--- MOGESP.ServiceLayer/Servicio/CuadroGeneralServicio.cs
+++ MOGESP.ServiceLayer/Servicio/CuadroGeneralServicio.cs
@@ -1,4 +1,5 @@
 ﻿using MOGESP.Entities.Dominio;
+using MOGESP.Entities.Utilidades;
 using MOGESP.DataAccess.TRAN.Datos;
 using System;
 using System.Collections.Generic;
@@ -42,9 +43,70 @@ namespace MOGESP.ServiceLayer.Servicio
         /// Este método retorna una lista que conforma la tabla de los primeros ingresos del cuadro general y puede ser filtrada por numero de convocatoria y numero de flujo.
         /// </summary>
         /// <returns>List<CuadroGeneral></returns>
-        public List<CuadroGeneral> traeCuadroGeneral(String numeroConvovatoria, int numeroFlujo)
+        public ContenedorCuadrogeneral traeCuadroGeneral(String numeroConvovatoria, int numeroFlujo)
         {
-            return cuadroGeneralDatos.traeCuadroGeneral(numeroConvovatoria, numeroFlujo);
+            List<CuadroGeneral> cuadroGeneral = cuadroGeneralDatos.traeCuadroGeneral(numeroConvovatoria, numeroFlujo);
+
+            int cantidadPruebasGH = 0;
+            int cantidadAntecedentes = 0;
+            int cantidadVialidad = 0;
+            int cantidadToxicologia = 0;
+            int cantidadPruebasMedicas = 0;
+            int cantidadPositivosEnEspera = 0;
+            int cantidadNegativos = 0;
+            int cantidadPendientes = 0;
+            int cantidadNombrados = 0;
+
+            foreach (CuadroGeneral tupla in cuadroGeneral)
+            {
+                if (tupla.CantidadDiasAdminPruebasGH != 0 ||tupla.CantidadDiasPruebasGH !=0 &&
+                 tupla.Condicion.Equals("Pendiente"))
+                {
+                    cantidadPruebasGH = cantidadPruebasGH + 1;
+                }
+                if(tupla.CantidadDiasAdminAntecedentes != 0 || tupla.CantidadDiasAntecedentes != 0 &&
+                 tupla.Condicion.Equals("Pendiente"))
+                {
+                    cantidadAntecedentes = cantidadAntecedentes + 1;
+                }
+                if (tupla.CantidadDiasAdminPruebasMedicas != 0 || tupla.CantidadDiasPruebasMedicas != 0 &&
+                 tupla.Condicion.Equals("Pendiente"))
+                {
+                    cantidadPruebasMedicas = cantidadPruebasMedicas + 1;
+                }
+                if (tupla.CantidadDiasAdminToxicologia != 0 || tupla.CantidadDiasToxicologia != 0 &&
+                  tupla.Condicion.Equals("Pendiente"))
+                {
+                    cantidadToxicologia = cantidadToxicologia + 1;
+                }
+                if (
+                    tupla.CantidadDiasAdminVialidad != 0 || tupla.CantidadDiasVialidad != 0 &&
+                    tupla.Condicion.Equals("Pendiente"))
+                {
+                    cantidadVialidad = cantidadVialidad + 1;
+                }
+                //--------------------------------------------------------------
+                if (tupla.Condicion.Equals("Positivo"))
+                {
+                    cantidadPositivosEnEspera = cantidadPositivosEnEspera + 1;
+                }
+                else if (tupla.Condicion.Equals("Negativo"))
+                {
+                    cantidadNegativos = cantidadNegativos + 1;
+                }
+                else if (tupla.Condicion.Equals("Pendiente"))
+                {
+                    cantidadPendientes = cantidadPendientes + 1;
+                }
+                else if (tupla.Condicion.Equals("Nombrado"))
+                {
+                    cantidadNombrados = cantidadNombrados + 1;
+                }
+            }
+            ContenedorCuadrogeneral contenedor = new ContenedorCuadrogeneral(cuadroGeneral,cantidadPruebasGH, cantidadAntecedentes
+                , cantidadVialidad, cantidadToxicologia, cantidadPruebasMedicas, cantidadPositivosEnEspera
+                , cantidadNegativos, cantidadPendientes, cantidadNombrados);
+            return contenedor;
         }
      }
 }
