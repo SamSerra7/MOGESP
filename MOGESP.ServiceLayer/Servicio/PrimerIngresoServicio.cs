ed7ce686dbc682f6d340934c30b19e8108361c4c
Matta-Aaron <37586057+Matta-Aaron@users.noreply.github.com>
2019-11-29 04:31:14 -0600

    PrimerIngreso_Update

--- MOGESP.ServiceLayer/Servicio/PrimerIngresoServicio.cs
+++ MOGESP.ServiceLayer/Servicio/PrimerIngresoServicio.cs
@@ -26,6 +26,10 @@ namespace MOGESP.ServiceLayer.Servicio
             return primerIngresoDatos.getAllPrimerosIngresos() ?? throw new Exception("No hay registros de primer ingreso");
         }
 
+        public PrimerIngreso getPrimerIngreso()
+        {
+            return primerIngresoDatos.getPrimerIngreso() ?? throw new Exception("No hay registros de primer ingreso");
+        }
 
         public void insertarPrimerIngreso(PrimerIngreso PI)
         {
