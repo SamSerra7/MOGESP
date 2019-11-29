522e29b1fffdb401e9f647bb18db8199d69933ca
Samuel <samsserranoguerra7@gmail.com>
2019-11-29 00:29:04 -0600

    Control FLujos add Logic

--- MOGESP.ServiceLayer/Servicio/HojaCitasPIServicio.cs
+++ MOGESP.ServiceLayer/Servicio/HojaCitasPIServicio.cs
@@ -27,6 +27,17 @@ namespace MOGESP.ServiceLayer.Servicio
             return hojaCitasDatos.getHojaCitasPI() ?? throw new Exception("No hay registros de citas de primer ingreso");
         }
 
+        /// <summary>
+        /// 28/11/2019
+        /// Samuel Serrano Guerra
+        /// Método que inserta una cita para un primer ingreso
+        /// </summary>
+        public void InsertarCitaPI(string cedula, string nombrePsi, DateTime fechaCita, int idAsist, string nombreEncargadoAdm)
+        {
 
+            if(string.IsNullOrEmpty(cedula) || string.IsNullOrEmpty(nombrePsi) || string.IsNullOrEmpty(nombreEncargadoAdm)) throw new Exception("No hay registros de citas de primer ingreso");
+
+            hojaCitasDatos.InsertarCitaPI(cedula, nombrePsi, fechaCita, idAsist, nombreEncargadoAdm); 
+        }
     }
 }
